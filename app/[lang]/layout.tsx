b4723d993416614bb8eb1cf815049ccfc7ab07b7
Diego Cruz <dactjs@icloud.com>
2024-04-01 17:16:52 -0400

    chore: add SpeedInsights component and update dependencies

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { theme } from "@/config/theme";
 import { PageParams } from "@/types/page-params";
@@ -38,7 +39,10 @@ export default function RootLayout({
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <ClientProviders>{children}</ClientProviders>
+            <ClientProviders>
+              {children}
+              <SpeedInsights />
+            </ClientProviders>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
