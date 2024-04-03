35f7e1785ea8c216b25fae88c615837e2925a93a
Diego Cruz <dactjs@icloud.com>
2024-04-03 11:34:43 -0400

    chore: fix drawer scroll

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { theme } from "@/config/theme";
@@ -39,6 +40,16 @@ export default function RootLayout({
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
+
+            <GlobalStyles
+              styles={{
+                "*": {
+                  scrollbarWidth: "thin",
+                  scrollbarColor: "chocolate transparent",
+                },
+              }}
+            />
+
             <ClientProviders>
               {children}
               <SpeedInsights />
