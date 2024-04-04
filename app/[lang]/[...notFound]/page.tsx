3c4a87a3169fda9130861cd4e923473f57f78591
Diego Cruz <dactjs@icloud.com>
2024-04-03 21:01:22 -0400

    chore: remove call-to-action on RootNotFound

--- app/[lang]/[...notFound]/page.tsx
+++ app/[lang]/[...notFound]/page.tsx
@@ -12,11 +12,9 @@ export interface RootNotFoundProps {
 export default async function RootNotFound({
   params: { lang },
 }: RootNotFoundProps) {
-  const {
-    not_found_heading,
-    not_found_description,
-    not_found_call_to_action_text,
-  } = await getDictionary(lang);
+  const { not_found_heading, not_found_description } = await getDictionary(
+    lang
+  );
 
   return (
     <Container
@@ -27,10 +25,6 @@ export default async function RootNotFound({
       <NotFound
         heading={not_found_heading}
         description={not_found_description}
-        callToAction={{
-          href: `/${lang}/dashboard`,
-          text: not_found_call_to_action_text,
-        }}
       />
     </Container>
   );
