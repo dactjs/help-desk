7b98c82217db3a1cee0d0ea27f83259519a83eb4
Diego Cruz <dicruz@qik.com.do>
2024-04-11 17:18:50 -0400

    feat: add view pages

---
+++ app/[lang]/admin/users/[user_id]/page.tsx
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2";
+
+import { Widget } from "@/components/templates/widget";
+import { replacePlaceholders } from "@/internationalization/utils/replace-placeholders";
+import { getShortUUID } from "@/utils/get-short-uuid";
+import { PageParams } from "@/types/page-params";
+
+import { getDictionary } from "./_dictionaries";
+
+type Params = PageParams & {
+  user_id: string;
+};
+
+export async function generateMetadata({
+  params: { lang, user_id },
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { title } = await getDictionary(lang);
+
+  const replaced = replacePlaceholders(title, {
+    id: getShortUUID(user_id),
+  });
+
+  return { title: replaced };
+}
+
+export default function UserPage() {
+  return (
+    <Container fixed sx={{ paddingY: 2 }}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid xs={12}>
+          <Widget />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
